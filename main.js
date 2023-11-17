46a850fd52ff01917f1801ebef186112160820e8
pistoleirodaluz <129514045+pistoleirodaluz@users.noreply.github.com>
2023-11-17 08:11:42 -0300

    /ç/;m/lç

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function Pom (){
     document.querySelector('#som_tecla_pom').play();
     }
     document.querySelector('.tecla_pom') .onclick = Pom;
+
+    function Clap (){
+        document.querySelector('#som_tecla_clap').play();
+        }
+        document.querySelector('.tecla_clap') .onclick = Clap;
