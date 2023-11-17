6f7c9e85d2288b7143583bf02b65c9bfc7419e2b
pistoleirodaluz <129514045+pistoleirodaluz@users.noreply.github.com>
2023-11-17 08:19:38 -0300

    ggh

--- main.js
+++ main.js
@@ -7,3 +7,38 @@ function Pom (){
         document.querySelector('#som_tecla_clap').play();
         }
         document.querySelector('.tecla_clap') .onclick = Clap;
+
+        function tim (){
+            document.querySelector('#som_tecla_tim').play();
+            }
+            document.querySelector('.tecla_tim') .onclick = tim;
+
+            function puff (){
+                document.querySelector('#som_tecla_puff').play();
+                }
+                document.querySelector('.tecla_puff') .onclick = puff;
+
+                function splash (){
+                    document.querySelector('#som_tecla_splash').play();
+                    }
+                    document.querySelector('.tecla_splash') .onclick = splash;
+
+                    function toin (){
+                        document.querySelector('#som_tecla_toin').play();
+                        }
+                        document.querySelector('.tecla_toin') .onclick = toin;
+
+                        function psh (){
+                            document.querySelector('#som_tecla_psh').play();
+                            }
+                            document.querySelector('.tecla_psh') .onclick = psh;
+
+                            function tic (){
+                                document.querySelector('#som_tecla_tic').play();
+                                }
+                                document.querySelector('.tecla_tic') .onclick = tic;
+
+                                function tom (){
+                                    document.querySelector('#som_tecla_tom').play();
+                                    }
+                                    document.querySelector('.tecla_tom') .onclick = tom;
