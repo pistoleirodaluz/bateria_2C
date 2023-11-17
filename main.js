a5af580e7ef82834dda024493ccdf443d2590ca4
pistoleirodaluz <129514045+pistoleirodaluz@users.noreply.github.com>
2023-11-17 08:41:50 -0300

    sdas

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 function Pom (){
     document.querySelector('#som_tecla_pom').play();
     }
-    document.querySelector('.tecla_pom') .onclick = Pom;
+    document.querySelector('.tecla_pom').onclick = Pom;
 
     function Clap (){
         document.querySelector('#som_tecla_clap').play();
         }
-        document.querySelector('.tecla_clap') .onclick = Clap;
+        document.querySelector('.tecla_clap').onclick = Clap;
 
         function tim (){
             document.querySelector('#som_tecla_tim').play();
             }
-            document.querySelector('.tecla_tim') .onclick = tim;
+            document.querySelector('.tecla_tim').onclick = tim;
 
             function puff (){
                 document.querySelector('#som_tecla_puff').play();
                 }
-                document.querySelector('.tecla_puff') .onclick = puff;
+                document.querySelector('.tecla_puff').onclick = puff;
 
                 function splash (){
                     document.querySelector('#som_tecla_splash').play();
                     }
-                    document.querySelector('.tecla_splash') .onclick = splash;
+                    document.querySelector('.tecla_splash').onclick = splash;
 
                     function toim (){
                         document.querySelector('#som_tecla_toim').play();
                         }
-                        document.querySelector('.tecla_toim') .onclick = toim;
+                        document.querySelector('.tecla_toim').onclick = toim;
 
                         function psh (){
                             document.querySelector('#som_tecla_psh').play();
                             }
-                            document.querySelector('.tecla_psh') .onclick = psh;
+                            document.querySelector('.tecla_psh').onclick = psh;
 
                             function tic (){
                                 document.querySelector('#som_tecla_tic').play();
                                 }
-                                document.querySelector('.tecla_tic') .onclick = tic;
+                                document.querySelector('.tecla_tic').onclick = tic;
 
                                 function tom (){
                                     document.querySelector('#som_tecla_tom').play();
                                     }
-                                    document.querySelector('.tecla_tom') .onclick = tom;
+                                    document.querySelector('.tecla_tom').onclick = tom;
